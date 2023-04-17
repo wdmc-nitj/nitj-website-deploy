4579adbb8db4f520d4394e41ae07398f9acdec3d
Vishal Mourya <90083566+MouryaVishal@users.noreply.github.com>
2023-04-17 15:53:54 +0530

    updatingTimeField

--- public/research/data/workshops.js
+++ public/research/data/workshops.js
@@ -68,14 +68,13 @@ fetch(`${data_url}` + 'research/events/get?visible=visible&upcoming=false&catego
         <!--date-->
         <div class="text-white font-bold text-4xl ">${d.getDate()}</div>
                   <div class="text-white font-normal text-2xl">${month[d.getMonth()].substring(0, 3)}</div>
-     
       </div>
     </div>
     <div class="w-full  lg:w-11/12 xl:w-full px-1 bg-white py-5 lg:px-2 lg:py-2 tracking-wide">
       <div class="flex flex-row lg:justify-start justify-center">
         <div class="text-gray-700 font-medium text-sm text-center lg:text-left px-2">
           <!--time-->
-          <i class="far fa-clock"></i> ${formatTime(update.dateTime.substring(11, 19))}
+          <i class="far fa-clock"></i> ${d.getHours()}:${d.getMinutes().toString().length == 1 ? 0 + d.getMinutes().toString() : d.getMinutes()}${d.getHours() < 12 ? "AM" : "PM"}
         </div>
         <div class="text-gray-700 font-medium text-sm text-center lg:text-left px-2">
           Organiser : ${update.organiser}
