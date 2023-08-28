90dfb7ee172abf9425e124c5ee371d74b1ba5468
Dr. D. Harimurugan <88608850+harimurugan1989@users.noreply.github.com>
2023-08-28 11:19:57 +0530

    Revert "Deployment"

--- models/Faculty.js
+++ models/Faculty.js
@@ -2,6 +2,10 @@ const mongoose=require('mongoose');
 const passport = require('passport');
 
 const Schema = new mongoose.Schema({
+    department: {
+        type: String,
+        enum: ['bt', 'ch', 'cy', 'ce', 'cse', 'ee', 'ece', 'hm', 'ipe', 'it', 'ice', 'ma', 'me', 'ph', 'tt', 'cf','cee','cai']
+    },
     name: {
         type: String
     },
@@ -370,6 +374,9 @@ const Schema = new mongoose.Schema({
         type:  String
     },
     show: { type: Boolean, default: true },
+    order: {
+        type: Number,
+    }
 }, { timestamps: true })
 
 module.exports = mongoose.model("Faculty", Schema);
