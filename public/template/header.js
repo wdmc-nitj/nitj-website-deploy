402ba8839d0cb00c41a86024ead23bc570bd6e96
Gautam Singla <ggsingla777@gmail.com>
2023-09-24 10:52:36 +0530

    Navbar Utils Optimized

--- public/template/header.js
+++ public/template/header.js
@@ -1,35 +1,32 @@
 import getNavbar from '../js/navbar.js'
 const bodyEl = document.body
 
-// <link rel="icon" type="image/x-icon" href="./public/assets/images/nitj-logo.png" />
-
 window.addEventListener('load', () => {
   handlingPDFs()
-  // document.head.appendChild('link')
-
-  const newFavIcon = document.createElement('link')
-  newFavIcon.setAttribute('rel', 'icon')
-  newFavIcon.setAttribute('type', 'image/x-icon')
-  newFavIcon.setAttribute('href', '/public/assets/images/nitj-logo.png')
-  document.head.appendChild(newFavIcon)
+  navbarUtil()
+  setFavicon('/public/assets/images/nitj-logo.png')
 })
-
+window.addEventListener('scroll', () => {
+  navbarUtil()
+})
 function handlingPDFs() {
-  const allLinks = [...document.getElementsByTagName('a')]
-  allLinks.forEach((link) => {
-    let currSrc =
-      link.getAttribute('href').includes('pdf') ||
-      link.getAttribute('href').includes('https')
-    // console.log(currSrc)
-    if (currSrc) {
-      // console.log(link)
-      link.setAttribute('target', '_blank')
-    }
-    // console.log(currSrc)
+  const linksToPDFs = document.querySelectorAll(
+    'a[href*="pdf"], a[href^="https"]'
+  )
+
+  linksToPDFs.forEach((link) => {
+    link.setAttribute('target', '_blank')
   })
 }
 
-window.onscroll = navbarUtil
+function setFavicon(iconPath) {
+  const newFavIcon = document.createElement('link')
+  newFavIcon.rel = 'icon'
+  newFavIcon.type = 'image/x-icon'
+  newFavIcon.href = iconPath
+  document.head.appendChild(newFavIcon)
+}
+
 fetch('/template/navbar.html')
   .then((res) => res.text())
   .then((html) => {
@@ -62,35 +59,29 @@ fetch('/template/navbar.html')
   })
 
 let slideIndex = 0
-function carousel() {
-  const x = document.getElementsByClassName('mySlides')
 
-  for (let slide of x) {
-    slide.style.display = 'none'
-  }
-  slideIndex += 1
-  if (slideIndex > x.length) {
-    slideIndex = 1
-  }
-  x[slideIndex - 1].style.display = 'block'
+function carousel() {
+  const slides = document.querySelectorAll('.mySlides')
+
+  slides.forEach((slide) => {
+    slide.style.transition = 'opacity 0.5s' // Adjust the transition duration as needed
+    slide.style.opacity = 0
+  })
+
+  // Update the slide index and display the next slide
+  slideIndex = (slideIndex + 1) % slides.length
+  slides[slideIndex].style.opacity = 1
+
+  // Automatically advance the carousel
   setTimeout(carousel, 2500)
 }
-
 function navbarUtil() {
-  const insti_name_cont = document.getElementById('instituteNameContainer')
   const institute_name = document.getElementsByClassName('institute_name')
-  const full_navbar = document.getElementsByTagName('header')
   const logo = document.getElementById('logo_250')
   const topbar = document.getElementById('top_bar')
   const diff_lang = document.getElementsByClassName('mySlides')
-  // animateValueIntiator()
-  // animateValueIntiatorInstitute()
-  // resAutoScroll()
 
   if (window.scrollY > 100) {
-    insti_name_cont.classList.remove('sm:h-20', 'sm:max-h-20')
-    insti_name_cont.classList.add('sm:h-10', 'sm:max-h-10')
-    // insti_name_cont.classList.add(' sm:max-h-20')
     topbar.style.transform = 'translateY(-50px)'
     for (let i = 0; i < diff_lang.length; i++) {
       diff_lang[i].classList.remove('mt-7')
@@ -100,9 +91,8 @@ function navbarUtil() {
         logo.style.width = '90px'
         logo.style.height = '90px'
       }
-      logo.classList.remove('top-9')
-      logo.classList.add('top-0', 'py-4')
-      logo.classList.remove('sm:translate-y-0')
+      logo.classList.remove('sm:translate-y-0', 'top-1')
+      logo.classList.add('sm:top-0')
       if (institute_name[i].classList.contains('sm:text-xl')) {
         institute_name[i].classList.remove('sm:text-xl')
         institute_name[i].classList.add('sm:text-lg')
@@ -114,19 +104,15 @@ function navbarUtil() {
       }
     }
   } else {
-    insti_name_cont.classList.remove('sm:h-10', 'sm:max-h-10')
-    insti_name_cont.classList.add('sm:h-20', 'sm:max-h-20')
     logo.style.width = '120px'
     logo.style.height = '120px'
     topbar.style.transform = 'translateY(0)'
     for (let i = 0; i < diff_lang.length; i++) {
       diff_lang[i].classList.add('mt-7')
     }
-    logo.classList.add('sm:translate-y-0')
-    logo.classList.add('top-9')
-    logo.classList.remove('top-0', 'py-4')
+    logo.classList.remove('sm:top-0')
+    logo.classList.add('sm:translate-y-0', 'top-1')
     for (var i = 0; i < institute_name.length; i++) {
-      // console.log(institute_name[i].classList);
       if (institute_name[i].classList.contains('sm:text-lg')) {
         institute_name[i].classList.remove('sm:text-lg')
         institute_name[i].classList.add('sm:text-xl')
