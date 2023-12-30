a14d55d24f3bb4d2f33e5c408cda1ad114ab8a63
Akleem khan <akleemkhan0208@gmail.com>
2023-12-31 00:19:15 +0530

    add marquee

--- public/js/utils/routingUtils.js
+++ public/js/utils/routingUtils.js
@@ -1,59 +1,59 @@
 export const getParams = () => {
   return window.location.search
-    .split('?')
+    .split("?")
     .filter((n) => n)
     .map((param) => {
-      return param.split('=')[1]
-    })
-}
+      return param.split("=")[1];
+    });
+};
 export function dataFilter(apiData) {
-  const data = apiData.filter((n) => n.showviewall === true)
+  const data = apiData.filter((n) => n.showviewall === true);
   data.sort((a, b) => {
-    return b.order - a.order
-  })
-  return data
+    return b.order - a.order;
+  });
+  return data;
 }
 
 export const dateManipulator = (data) => {
-  const date = new Date(data)
-  const year = date.getFullYear()
-  const day = date.getDate()
+  const date = new Date(data);
+  const year = date.getFullYear();
+  const day = date.getDate();
   const months = [
-    'JAN',
-    'FEB',
-    'MAR',
-    'APR',
-    'MAY',
-    'JUNE',
-    'JULY',
-    'AUG',
-    'SEPT',
-    'OCT',
-    'NOV',
-    'DEC',
-  ]
-  const month = months[date.getMonth()]
-  const FullDate = month + ' ' + day + ',' + year
-  return FullDate
-}
+    "JAN",
+    "FEB",
+    "MAR",
+    "APR",
+    "MAY",
+    "JUNE",
+    "JULY",
+    "AUG",
+    "SEPT",
+    "OCT",
+    "NOV",
+    "DEC",
+  ];
+  const month = months[date.getMonth()];
+  const FullDate = month + " " + day + "," + year;
+  return FullDate;
+};
 
 export function putCategory(category) {
-  const titlePlaceholders = document.getElementsByClassName('page-title')
+  const titlePlaceholders = document.getElementsByClassName("page-title");
 
   for (const item of titlePlaceholders) {
-    item.innerHTML = normalCaseMaker(category)
+    item.innerHTML = normalCaseMaker(category);
   }
 }
 
 export function pageTitleUpdater(category, title) {
-  const updatedCategory = normalCaseMaker(category)
-  document.title = `NITJ`
+  const updatedCategory = normalCaseMaker(category);
+  document.title = `NITJ`;
 }
 
 export function normalCaseMaker(str) {
-  return str.replace(/([A-Z])/g, ' $1').replace(/^./, function (str) {
-    return str.toUpperCase()
-  })
+  return str.replace(/([A-Z])/g, " $1").replace(/^./, function (str) {
+    return str.toUpperCase();
+  });
 }
 
 // function titleManipulator(title) {
