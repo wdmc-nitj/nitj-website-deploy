63efd7983ed69e98407223064eedb2b2d1fbebd7
nitjofficial <78634968+nitjofficial@users.noreply.github.com>
2024-08-20 13:10:15 +0530

    Update script.js

--- public/today/script.js
+++ public/today/script.js
@@ -13,9 +13,12 @@ async function fetchEvents() {
     const events = [];
 
     for (let i = 0; i < 3; i++) {
-      const currentYear = year + Math.floor((month + i - 1) / 12);
-      const currentMonth = (month + i) % 12 || 12;
+      // const currentYear = year + Math.floor((month + i - 1) / 12);
+      // const currentMonth = (month + i) % 12 || 12;
       
+      const monthOffset = month + i - 1;
+      const currentYear = year + Math.floor(monthOffset / 12);
+      const currentMonth = (monthOffset % 12) + 1;
 
       const response = await fetch(`${getevents}?year=${currentYear}&month=${currentMonth}`);
       const data = await response.json();
