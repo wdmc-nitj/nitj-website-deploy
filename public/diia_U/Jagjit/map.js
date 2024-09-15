2f8644d1a21374ead9177de354c765c313fb5a84
Jagjit <146818104+Jagjit0306@users.noreply.github.com>
2024-09-15 23:15:34 +0530

    Updated maps + testimonial

--- public/diia_U/Jagjit/map.js
+++ public/diia_U/Jagjit/map.js
@@ -1,31 +1,36 @@
 // const {countryData} = require('./countriesGJSON/js')
 import countryData from './countriesGJSON.js'
 
-const dataSet = [
-    {
-        country: 'IN',
-        color: '#00ff00',
-        text: 'Text ABC'
-    }
+let dataSet = [
+    // {
+    //     country: 'IN',
+    //     color: '#00ff00',
+    //     maptext: 'Text ABC, India'
+    // }
 ]
-const dataSet2 = [
-    {
-        country: 'CA',
-        color:'#0000ff',
-        text: 'Text ABC'
-    }
+let dataSet2 = [
+    // {
+    //     country: 'CA',
+    //     color:'#0000ff',
+    //     maptext: 'Text ABC, Canada'
+    // }
 ]
-const dataSet3 = [
-    {
-        country: 'ZM',
-        color: '#ff0000',
-        text: 'Text ABC'
-    }
+let dataSet3 = [
+    // {
+    //     country: 'ZM',
+    //     color: '#ff0000',
+    //     maptext: 'Text ABC, Zambia'
+    // }
 ]
 
 function getColor(val, dataS) {
     for(const data of dataS)
         if(data.country == val) return data.color
+}
+
+function getText(val, dataS) {
+    for(const data of dataS)
+        if(data.country == val) return data.maptext
 }
 
 function GJSONGenerator(dataS) {
@@ -39,7 +44,7 @@ function GJSONGenerator(dataS) {
     return {
         "type": "FeatureCollection",
         "features": countryData["features"].filter(e=>{
-            return checker(e['properties']['ISO_A2'])
+            return checker(e['properties']['ADMIN'])
         })
     }
 }
@@ -83,8 +88,8 @@ window.onload = async function() {
     function SetColours(mp, CDS) {
         function style(feature) {
             return {
-                'fillColor': getColor(feature.properties.ISO_A2, CDS),
-                'color': getColor(feature.properties.ISO_A2, CDS),
+                'fillColor': getColor(feature.properties.ADMIN, CDS),
+                'color': getColor(feature.properties.ADMIN, CDS),
                 'weight': 0.2,
                 'opacity': 0.8
             };
@@ -95,7 +100,7 @@ window.onload = async function() {
                 layer.on({
                     mouseover: function(e) {
                         var layer0 = e.target;
-                        layer0.bindTooltip(feature.properties.ADMIN, {permanent:false, direction: 'top'}).openTooltip()
+                        layer0.bindTooltip(getText(feature.properties.ADMIN, CDS), {permanent:false, direction: 'top'}).openTooltip()
                     },
                     mouseout: function (e) {
                         e.target.closeTooltip();
@@ -105,7 +110,6 @@ window.onload = async function() {
         }).addTo(mp)
     }
 
-    // console.log('t',window.location)
     try {
         const response = await fetch(`https://nitjfinal.onrender.com/api/diia/maps`, {
             method: 'GET',
@@ -116,13 +120,20 @@ window.onload = async function() {
         if(response) {
             const data = await response.json()
             if(data) console.log(data)
+
+            dataSet = data.filter(e=>e.type=='student')
+            dataSet2 = data.filter(e=>e.type=='research')
+            dataSet3 = data.filter(e=>e.type=='alumni')
+            SetColours(map, dataSet)
+            SetColours(map2, dataSet2)
+            SetColours(map3, dataSet3)
         }
     }
     catch(e) {
         console.log(e)
     }
 
-    SetColours(map, dataSet)
-    SetColours(map2, dataSet2)
-    SetColours(map3, dataSet3)
+    // SetColours(map, dataSet)
+    // SetColours(map2, dataSet2)
+    // SetColours(map3, dataSet3)
 }
