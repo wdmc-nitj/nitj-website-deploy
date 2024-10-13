53d34068a00bf7f7612960fc960f614671baa237
Dr. D. Harimurugan <88608850+harimurugan1989@users.noreply.github.com>
2024-10-14 00:32:10 +0530

    Update script.js
    month increased to 5

--- public/today/script.js
+++ public/today/script.js
@@ -12,7 +12,7 @@ async function fetchEvents() {
 
     const events = [];
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 5; i++) {
       const monthOffset = month + i - 1;
       const currentYear = year + Math.floor(monthOffset / 12);
     
